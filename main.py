b2d39993d08c9b1d023ca73a3219f9f561f81a03
RCEmilio <eruiz@gbmpaulovi.edu.ec>
2025-10-22 16:33:21 -0500

    Agregar main.py
    Realicé un programa de adivinar un número hecho en phyton

---
+++ main.py
@@ -0,0 +1,17 @@
+import random
+
+numero_secreto = random.randint(1,50);
+print(numero_secreto)
+intento = 0 
+adivinado = 0 
+
+#Ciclo repetitivo 
+while adivinado != numero_secreto:
+     #Intentos (Incrementos y decrementos)
+     intento += 1 
+     print(F"Este es tu intento {intento}, adivina el numero entre 1 y 50")
+     adivinado = int(input("Ingrese el numero "))
+     if intento == 5: 
+        print("LENTEJAS, demasiados intentos amigo")
+        print ("A perdido")
+        break
